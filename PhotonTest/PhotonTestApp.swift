3d373136907349cb2414e52ee8be27953490171f
mayurnilwant <mayurnilwant@gmail.com>
2024-01-30 16:05:59 -0500

    Initial Commit

---
+++ PhotonTest/PhotonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonTestApp.swift
+//  PhotonTest
+//
+//  Created by Mayur Nilwant on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
